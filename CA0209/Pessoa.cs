01c402a84f974a5093b49c54b6f3a3301abe5f86
Ninadilha <vitor.padilha2@etec.sp.gov.br>
2025-09-03 17:38:12 -0300

    Adicionamento da classe pessoa e seus trem ai

--- CA0209/Pessoa.cs
+++ CA0209/Pessoa.cs
@@ -8,5 +8,29 @@ namespace CA0209
 {
     internal class Pessoa
     {
+        protected string cpf;
+        protected string nome;
+        protected string endereco;
+        protected string email;
+
+        public void cadastrar()
+        {
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite seu Endereço");
+            endereco = Console.ReadLine();
+            Console.WriteLine("Digite seu email");
+            email = Console.ReadLine();
+            Console.WriteLine("Digite seu cpf");
+            cpf = Console.ReadLine();
+        }
+
+        public void consultar()
+        {
+            Console.WriteLine("Olá {0}", nome);
+            Console.WriteLine("cpf:{0}\n email:{1}\n Endereço:{2}\n", cpf, email, endereco);
+        }
+
     }
+
 }
